80d1d3c69507225127bc5f5b3c91a761e82b4c48
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2022-08-31 12:04:51 +0900

    basic implementation of Hello Kit with Swift based and Objective-C based
    deployment from iOS 11.0

---
+++ HelloKit2/ObjcHello.h
@@ -0,0 +1,18 @@
+//
+//  ObjcHello.h
+//  HelloKit2
+//
+//  Created by Kaz Yoshikawa on 8/31/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjcHello : NSObject
+
+- (void)hello;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HelloKit2/SwiftyHello.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftyHello.swift
+//  HelloKit2
+//
+//  Created by Kaz Yoshikawa on 8/31/22.
+//
+
+import Foundation
+
+open class SwiftyHello {
+    func hello() {
+		print("Hello from Swift")
+    }
+}
